caf01e09e03a1f3cbe088241d3eeaf97e0f34b3b
Harihara sudhan. R <133210562+Harihara04sudhan@users.noreply.github.com>
2024-03-25 10:20:36 +0530

    new changes in login page

---
+++ login page/login_script.js
@@ -0,0 +1,14 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const signInForm = document.getElementById('signInForm');
+const signUpForm = document.getElementById('signUpForm');
+
+signUpButton.addEventListener('click', () => {
+    signUpForm.style.display = 'block';
+    signInForm.style.display = 'none';
+});
+
+signInButton.addEventListener('click', () => {
+    signInForm.style.display = 'block';
+    signUpForm.style.display = 'none';
+});
